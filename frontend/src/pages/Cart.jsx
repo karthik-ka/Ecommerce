8a9dcbb76c40b476a2f14189d599a5a7675d1289
karthik-ka <karthik.ka2001@gmail.com>
2023-08-02 16:07:18 +0530

    backend commected

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -19,17 +19,17 @@ const Cart = () => {
 
    return (
    <section className="max-w-[90vw] md:max-w-[700px] lg:max-w-[900px] xl:max-w-[1280px] m-auto py-16">
-      <div className='flex gap-4 justify-between'>
+      <div className='flex flex-col lg:flex-row gap-4 justify-between'>
 
          {/* =================================left section======================================= */}
 
-         <div className='px-6 flex-1'>
+         <div className='md:px-6 flex-1'>
             <div className='flex pb-6 justify-between'>
                <h1 className='font-semibold text-2xl'>Cart</h1>
-               <span className='text-sm font-semibold mr-8'><DeleteOutlineOutlinedIcon fontSize='10'/>Clear cart</span>
+               <span className='text-sm font-semibold'><DeleteOutlineOutlinedIcon fontSize='10'/>Clear cart</span>
             </div>
             <table className='w-full'>
-               <tr className='text-eco-grey'>
+               <tr className='text-eco-grey border-b-2 border-eco-light-grey'>
                   <th className='float-left'>PRODUCTS</th>
                   <th>QUANTITY</th>
                   <th>PRICE</th>
@@ -37,10 +37,10 @@ const Cart = () => {
 
                {data.map((item)=>{
                   return (
-                     <tr key={item.id} className='text-center text-eco-light-black' >
-                        <td className='flex gap-3 py-2'> 
+                     <tr key={item.id} className='text-center text-eco-light-black border-b-2 border-eco-light-grey'>
+                        <td className='flex flex-col md:flex-row gap-3 py-4'> 
                            <img src={item.image} alt="" className='w-24 h-24 object-cover rounded-lg' />
-                           <h2 className='font-semibold'>{item.title}</h2>
+                           <h2 className='font-semibold hidden md:block'>{item.title}</h2>
                         </td>
                         <td >
                            <button className='px-3 py-1 rounded-sm mx-1 bg-eco-light-grey' onClick={handleDec}>-</button>
@@ -60,7 +60,7 @@ const Cart = () => {
 
          {/* =================================right section======================================= */}
 
-         <div className='md:w-80 h-max py-4 border border-eco-light-grey shadow-lg rounded-xl'>
+         <div className='lg:w-80 h-max py-4 border border-eco-light-grey shadow-lg rounded-xl'>
             <div className='flex justify-between px-4 text-sm text-eco-grey font-medium'>
                <h2>Subtotal</h2>
                <p className='text-eco-light-black font-semibold'>₹{data[0].price}</p>
